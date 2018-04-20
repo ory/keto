a0d2b2212078ee9811f0dff58e6a7ae94e3d7037
arekkas <aeneas@ory.am>
2018-04-20 22:26:36 +0200

    Adds SDK helpers

--- sdk/go/keto/sdk.go
+++ sdk/go/keto/sdk.go
@@ -1,7 +1,44 @@
 package keto
 
+import (
+	"strings"
+
+	"github.com/ory/keto/sdk/go/keto/swagger"
+	"github.com/pkg/errors"
+)
+
 type SDK interface {
 	RoleSDK
 	WardenSDK
 	PolicySDK
+}
+
+type CodeGenSDK struct {
+	*swagger.RoleApi
+	*swagger.WardenApi
+	*swagger.PolicyApi
+
+	Configuration *Configuration
+}
+
+// Configuration configures the CodeGenSDK.
+type Configuration struct {
+	// EndpointURL should point to the url of ORY Hydra, for example: http://localhost:4444
+	EndpointURL string
+}
+
+// CodeGenSDK instantiates a new CodeGenSDK instance or returns an error.
+func NewCodeGenSDK(c *Configuration) (*CodeGenSDK, error) {
+	if c.EndpointURL == "" {
+		return nil, errors.New("Please specify the ORY Keto endpoint URL")
+	}
+
+	c.EndpointURL = strings.TrimRight(c.EndpointURL, "/")
+	sdk := &CodeGenSDK{
+		RoleApi:   swagger.NewRoleApiWithBasePath(c.EndpointURL),
+		WardenApi: swagger.NewWardenApiWithBasePath(c.EndpointURL),
+		PolicyApi: swagger.NewPolicyApiWithBasePath(c.EndpointURL),
+	}
+
+	return sdk, nil
 }
