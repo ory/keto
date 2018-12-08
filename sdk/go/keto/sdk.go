d840faadaf4a4ff3c30205609d9975803058a4d3
aeneasr <aeneas@ory.sh>
2018-12-08 15:09:12 +0100

    sdk: Add go SDK interface
    Signed-off-by: aeneasr <aeneas@ory.sh>

--- sdk/go/keto/sdk.go
+++ sdk/go/keto/sdk.go
@@ -8,6 +8,22 @@ import (
 	"github.com/ory/keto/sdk/go/keto/swagger"
 )
 
+type SDK interface {
+	AddOryAccessControlPolicyRoleMembers(flavor string, id string, body swagger.AddOryAccessControlPolicyRoleMembersBody) (*swagger.OryAccessControlPolicyRole, *swagger.APIResponse, error)
+	DeleteOryAccessControlPolicy(flavor string, id string) (*swagger.APIResponse, error)
+	DeleteOryAccessControlPolicyRole(flavor string, id string) (*swagger.APIResponse, error)
+	DoOryAccessControlPoliciesAllow(flavor string, body swagger.OryAccessControlPolicyAllowedInput) (*swagger.AuthorizationResult, *swagger.APIResponse, error)
+	GetOryAccessControlPolicy(flavor string, id string) (*swagger.OryAccessControlPolicy, *swagger.APIResponse, error)
+	GetOryAccessControlPolicyRole(flavor string, id string) (*swagger.OryAccessControlPolicyRole, *swagger.APIResponse, error)
+	ListOryAccessControlPolicies(flavor string, limit int64, offset int64) ([]swagger.OryAccessControlPolicy, *swagger.APIResponse, error)
+	ListOryAccessControlPolicyRoles(flavor string, limit int64, offset int64) ([]swagger.OryAccessControlPolicyRole, *swagger.APIResponse, error)
+	RemoveOryAccessControlPolicyRoleMembers(flavor string, id string, body swagger.RemoveOryAccessControlPolicyRoleMembersBody) (*swagger.APIResponse, error)
+	UpsertOryAccessControlPolicy(flavor string, body swagger.OryAccessControlPolicy) (*swagger.OryAccessControlPolicy, *swagger.APIResponse, error)
+	UpsertOryAccessControlPolicyRole(flavor string, body swagger.OryAccessControlPolicyRole) (*swagger.OryAccessControlPolicyRole, *swagger.APIResponse, error)
+}
+
+var _ SDK = new(CodeGenSDK)
+
 type CodeGenSDK struct {
 	*swagger.EnginesApi
 
