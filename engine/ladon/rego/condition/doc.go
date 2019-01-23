fee4f7f7e5650ae6cabfed7ee37135a7f9feb3af
JAIN <sumeejai@C02XCDBXJG5J.local>
2019-01-23 10:23:17 -0500

    Added doc.go in all rego subfolders to force download the folder when we run go mod vendor
    Signed-off-by: Sumeet Jain <sumeet.jain44@gmail.com>

--- engine/ladon/rego/condition/doc.go
+++ engine/ladon/rego/condition/doc.go
@@ -1,5 +1,2 @@
 // Package rego forces go mod vendor to download rego folder to the vendor directory
 package rego
-
-// Define placeholder type to add this file to buildable go files
-type rego []string
