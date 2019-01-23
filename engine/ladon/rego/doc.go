0c3a3941348f3897b6004311415d5c8c9916cf6f
Sumeet Jain <sumeet.jain44@gmail.com>
2019-01-23 14:02:12 -0500

    Added placeholder go files in rego folder to force download rego folder when we run go mod>
    Signed-off-by: Sumeet Jain <sumeet.jain44@gmail.com>

---
+++ engine/ladon/rego/doc.go
@@ -0,0 +1,9 @@
+// Package rego is a placeholder package to force download this package and its subpackages when we run `go mod vendor`
+package rego
+
+import (
+	_ "github.com/ory/keto/engine/ladon/rego/condition"
+	_ "github.com/ory/keto/engine/ladon/rego/core"
+	_ "github.com/ory/keto/engine/ladon/rego/exact"
+	_ "github.com/ory/keto/engine/ladon/rego/regex"
+)
