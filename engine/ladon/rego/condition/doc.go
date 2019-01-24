59cc05328f068fc3046b2dbc022a562fd5d67960
sumeet jain <sumeet.jain44@gmail.com>
2019-01-24 04:53:23 -0500

    gomod: Ex- and import rego subdirectories for `go get` #77
    Closes #73
    
    Signed-off-by: Sumeet Jain <sumeet.jain44@gmail.com>

---
+++ engine/ladon/rego/condition/doc.go
@@ -0,0 +1,2 @@
+// Package condition is placeholder package to force download this package when we run `go mod vendor`
+package condition
---
+++ engine/ladon/rego/core/doc.go
@@ -0,0 +1,2 @@
+// Package core is placeholder package to force download this package when we run `go mod vendor`
+package core
