08fc35044bd491430e0bda37265e0d97d83663ba
Rob Liebowitz <rliebowitz@morningconsult.com>
2019-01-28 15:35:15 -0500

    cmd: Import glob in rego/doc.go
    Signed-off-by: Rob Liebowitz <rliebowitz@morningconsult.com>

--- engine/ladon/rego/doc.go
+++ engine/ladon/rego/doc.go
@@ -5,5 +5,6 @@ import (
 	_ "github.com/ory/keto/engine/ladon/rego/condition"
 	_ "github.com/ory/keto/engine/ladon/rego/core"
 	_ "github.com/ory/keto/engine/ladon/rego/exact"
+	_ "github.com/ory/keto/engine/ladon/rego/glob"
 	_ "github.com/ory/keto/engine/ladon/rego/regex"
 )
