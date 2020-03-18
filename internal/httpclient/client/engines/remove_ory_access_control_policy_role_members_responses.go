06cee83d56240c0aa62182b7d2906dc52dddd716
aeneasr <aeneas@ory.sh>
2020-03-18 18:35:21 +0000

    chore: Regenerate swagger spec and internal client

--- internal/httpclient/client/engines/remove_ory_access_control_policy_role_members_responses.go
+++ internal/httpclient/client/engines/remove_ory_access_control_policy_role_members_responses.go
@@ -10,9 +10,8 @@ import (
 	"io"
 
 	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
-
-	strfmt "github.com/go-openapi/strfmt"
 )
 
 // RemoveOryAccessControlPolicyRoleMembersReader is a Reader for the RemoveOryAccessControlPolicyRoleMembers structure.
--- internal/httpclient/client/version/get_version_responses.go
+++ internal/httpclient/client/version/get_version_responses.go
@@ -10,8 +10,7 @@ import (
 	"io"
 
 	"github.com/go-openapi/runtime"
-
-	strfmt "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/strfmt"
 
 	"github.com/ory/keto/internal/httpclient/models"
 )
