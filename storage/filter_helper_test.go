2ff0815cc260380f0102464ef57169924243d547
xlanor <contact@jingk.ai>
2020-10-02 11:10:00 +0800

    Now, instead of returning <nil>, it should return an empty *[]storage.Policy/Role{}

--- storage/filter_helper_test.go
+++ storage/filter_helper_test.go
@@ -52,7 +52,7 @@ var (
 				Members: []string{"mem1", "mem2"},
 			},
 		},
-		nil,
+		[]Role{},
 		rolReq,
 		rolReq,
 		rolReq,
@@ -93,7 +93,7 @@ var (
 				Resources: []string{"res1", "res2"},
 			},
 		},
-		nil,
-		nil,
+		[]Policy{},
+		[]Policy{},
 	}
 )
