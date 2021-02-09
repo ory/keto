53e177473b17e4a769d952fed2738cfde7e0dc7c
zepatrik <zepatrik@users.noreply.github.com>
2021-02-09 15:46:54 +0100

    feat: allow to migrate everything at once

---
+++ cmd/migrate/migrate_test.go
@@ -0,0 +1,145 @@
+package migrate
+
+import (
+	"bytes"
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/ory/x/cmdx"
+	"github.com/ory/x/configx"
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ory/keto/internal/driver"
+	"github.com/ory/keto/internal/driver/config"
+	"github.com/ory/keto/internal/namespace"
+	"github.com/ory/keto/internal/x"
+)
+
+func assertAllApplied(t *testing.T, status string) {
+	assert.NotContains(t, status, "Pending")
+	assert.Contains(t, status, "Applied")
+}
+
+func assertNoneApplied(t *testing.T, status string) {
+	assert.Contains(t, status, "Pending")
+	assert.NotContains(t, status, "Applied")
+}
+
+func TestMigrate(t *testing.T) {
+	nspaces := []*namespace.Namespace{
+		{
+			Name: "default",
+			ID:   0,
+		},
+		{
+			Name: "other",
+			ID:   1,
+		},
+	}
+
+	newCmd := func(ctx context.Context, persistentArgs ...string) *cmdx.CommandExecuter {
+		return &cmdx.CommandExecuter{
+			New: func() *cobra.Command {
+				cmd := newMigrateCmd()
+				configx.RegisterFlags(cmd.PersistentFlags())
+				return cmd
+			},
+			Ctx:            ctx,
+			PersistentArgs: persistentArgs,
+		}
+	}
+
+	for _, dsn := range x.GetDSNs(t) {
+		if dsn.Name == "memory" {
+			t.Run("dsn=memory", func(t *testing.T) {
+				t.Run("case=auto migrates", func(t *testing.T) {
+					hook := &test.Hook{}
+					ctx := context.WithValue(context.Background(), driver.LogrusHookContextKey, hook)
+
+					cf := x.ConfigFile(t, map[string]interface{}{
+						config.KeyDSN:        dsn.Conn,
+						config.KeyNamespaces: nspaces,
+						"log.level":          "debug",
+					})
+
+					cmd := newCmd(ctx, "-c", cf)
+
+					out := cmd.ExecNoErr(t, "up", "--"+FlagYes)
+					assert.Contains(t, out, "All migrations are already applied, there is nothing to do.")
+				})
+			})
+		} else {
+			t.Run("dsn="+dsn.Name, func(t *testing.T) {
+				hook := &test.Hook{}
+				ctx := context.WithValue(context.Background(), driver.LogrusHookContextKey, hook)
+
+				cf := x.ConfigFile(t, map[string]interface{}{
+					config.KeyDSN:        dsn.Conn,
+					config.KeyNamespaces: nspaces,
+					"log.level":          "debug",
+				})
+
+				cmd := newCmd(ctx, "-c", cf)
+
+				t.Run("case=aborts on no", func(t *testing.T) {
+					stdOut, stdErr, err := cmd.Exec(bytes.NewBufferString("n\n"), "up")
+					require.NoError(t, err, "%s %s", stdOut, stdErr)
+
+					assert.Contains(t, stdOut, "Pending", "%s %s", stdOut, stdErr)
+					assert.NotContains(t, stdOut, "Applied", "%s %s", stdOut, stdErr)
+					assert.Contains(t, stdOut, "Aborting", "%s %s", stdOut, stdErr)
+				})
+
+				t.Run("case=applies on yes input", func(t *testing.T) {
+					stdOut, stdErr, err := cmd.Exec(bytes.NewBufferString("y\n"), "up")
+					require.NoError(t, err, "%s %s", stdOut, stdErr)
+
+					t.Cleanup(func() {
+						// migrate all down
+						cmd.ExecNoErr(t, "down", "0")
+					})
+
+					parts := strings.Split(stdOut, "Do you want to apply above planned migrations?")
+					require.Len(t, parts, 2)
+
+					assertNoneApplied(t, parts[0])
+					assertAllApplied(t, parts[1])
+				})
+
+				t.Run("case=applies on yes flag", func(t *testing.T) {
+					out := cmd.ExecNoErr(t, "up", "--"+FlagYes)
+
+					t.Cleanup(func() {
+						// migrate all down
+						cmd.ExecNoErr(t, "down", "0")
+					})
+
+					parts := strings.Split(out, "Applying migrations...")
+					require.Len(t, parts, 2)
+
+					assertNoneApplied(t, parts[0])
+					assertAllApplied(t, parts[1])
+				})
+
+				t.Run("case=applies namespace migrations on flag", func(t *testing.T) {
+					out := cmd.ExecNoErr(t, "up", "--"+FlagYes, "--"+FlagAllNamespace)
+
+					t.Cleanup(func() {
+						// migrate all down
+						cmd.ExecNoErr(t, "down", "0")
+					})
+
+					parts := strings.Split(out, "Applying migrations...")
+					require.Len(t, parts, 2)
+
+					assertNoneApplied(t, parts[1])
+					assertAllApplied(t, parts[0])
+				})
+			})
+		}
+	}
+}
--- cmd/migrate/up.go
+++ cmd/migrate/up.go
@@ -1,18 +1,26 @@
 package migrate
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
 
 	"github.com/ory/x/cmdx"
-	"github.com/ory/x/flagx"
 	"github.com/spf13/cobra"
 
 	"github.com/ory/keto/internal/driver"
 )
 
-const FlagYes = "yes"
+const (
+	FlagYes          = "yes"
+	FlagAllNamespace = "all-namespaces"
+)
 
 func newUpCmd() *cobra.Command {
+	var yes, allNamespaces bool
+
 	cmd := &cobra.Command{
 		Use: "up",
 		RunE: func(cmd *cobra.Command, _ []string) error {
@@ -22,25 +30,75 @@ func newUpCmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			if err := reg.Migrator().MigrationStatus(ctx, cmd.OutOrStdout()); err != nil {
-				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Could not get migration status: %+v\n", err)
+
+			status := &bytes.Buffer{}
+			if err := reg.Migrator().MigrationStatus(ctx, status); err != nil {
+				_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "Could not get migration status: %+v\n", err)
 				return cmdx.FailSilently(cmd)
 			}
+			_, _ = cmd.OutOrStdout().Write(status.Bytes())
 
-			if !flagx.MustGetBool(cmd, FlagYes) && !cmdx.AskForConfirmation("Do you want to apply above planned migrations?", cmd.InOrStdin(), cmd.OutOrStdout()) {
+			if !strings.Contains(status.String(), "Pending") {
+				_, _ = fmt.Fprintln(cmd.OutOrStdout(), "All migrations are already applied, there is nothing to do.")
+				return nil
+			}
+
+			if !yes && !cmdx.AskForConfirmation("Do you want to apply above planned migrations?", cmd.InOrStdin(), cmd.OutOrStdout()) {
 				_, _ = fmt.Fprintln(cmd.OutOrStdout(), "Aborting")
 				return nil
 			}
 
+			_, _ = fmt.Fprintln(cmd.OutOrStdout(), "Applying migrations...")
+
 			if err := reg.Migrator().MigrateUp(ctx); err != nil {
-				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Could not apply migrations: %+v\n", err)
+				_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "Could not apply migrations: %+v\n", err)
 				return cmdx.FailSilently(cmd)
 			}
+
+			if err := reg.Migrator().MigrationStatus(ctx, cmd.OutOrStdout()); err != nil {
+				_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "Could not get migration status: %+v\n", err)
+				return cmdx.FailSilently(cmd)
+			}
+
+			if !allNamespaces {
+				// everything is done already
+				return nil
+			}
+
+			nm, err := reg.Config().NamespaceManager()
+			if err != nil {
+				return errors.Wrap(err, "could not get the namespace manager")
+			}
+
+			nspaces, err := nm.Namespaces(cmd.Context())
+			if err != nil {
+				_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "Could not get the namespaces: %+v\n", err)
+				return cmdx.FailSilently(cmd)
+			}
+
+			for _, nspace := range nspaces {
+				if err := reg.NamespaceMigrator().NamespaceStatus(cmd.Context(), cmd.OutOrStdout(), nspace); err != nil {
+					_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "Could not get migration status for namespace %s: %+v\n", nspace.Name, err)
+					return cmdx.FailSilently(cmd)
+				}
+
+				if !yes && !cmdx.AskForConfirmation("Do you want to apply above planned migrations?", cmd.InOrStdin(), cmd.OutOrStdout()) {
+					_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Skipping namespace %s\n", nspace.Name)
+					continue
+				}
+
+				if err := reg.NamespaceMigrator().MigrateNamespaceUp(cmd.Context(), nspace); err != nil {
+					_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "Could not apply namespace migrations for namespace %s: %+v\n", nspace.Name, err)
+					return cmdx.FailSilently(cmd)
+				}
+			}
+
 			return nil
 		},
 	}
 
-	cmd.Flags().BoolP(FlagYes, "y", false, "yes to all questions, no user input required")
+	cmd.Flags().BoolVarP(&yes, FlagYes, "y", false, "yes to all questions, no user input required")
+	cmd.Flags().BoolVar(&allNamespaces, FlagAllNamespace, false, "migrate all pending namespaces as well")
 
 	return cmd
 }
