c7d2770ed570238fd1262bcc4e5b4afa6c12d80e
Patrik <zepatrik@users.noreply.github.com>
2021-02-10 14:49:47 +0100

    fix: secure query building (#442)

--- go.mod
+++ go.mod
@@ -17,7 +17,7 @@ require (
 	github.com/go-openapi/swag v0.19.5
 	github.com/go-openapi/validate v0.19.3
 	github.com/go-swagger/go-swagger v0.21.1-0.20200107003254-1c98855b472d
-	github.com/gobuffalo/pop/v5 v5.3.1
+	github.com/gobuffalo/pop/v5 v5.3.3
 	github.com/golang/protobuf v1.4.3
 	github.com/gorilla/sessions v1.1.3
 	github.com/gorilla/websocket v1.4.2
--- internal/persistence/sql/pagination_test.go
+++ internal/persistence/sql/pagination_test.go
@@ -13,37 +13,37 @@ import (
 
 func TestPaginationToken(t *testing.T) {
 	for i, tc := range []struct {
-		size           int
-		token          string
-		expectedErr    error
-		expectedOffset int
-		expectedLimit  int
+		size            int
+		token           string
+		expectedErr     error
+		expectedPage    int
+		expectedPerPage int
 	}{
 		{
-			size:           10,
-			token:          "10",
-			expectedOffset: 10,
-			expectedLimit:  10,
+			size:            10,
+			token:           "10",
+			expectedPage:    10,
+			expectedPerPage: 10,
 		},
 		{
-			size:           0,
-			token:          "15",
-			expectedOffset: 15,
-			expectedLimit:  defaultPageSize,
+			size:            0,
+			token:           "15",
+			expectedPage:    15,
+			expectedPerPage: defaultPageSize,
 		},
 		{
-			size:          0,
-			token:         "-15",
-			expectedErr:   persistence.ErrMalformedPageToken,
-			expectedLimit: defaultPageSize,
+			size:            0,
+			token:           "-15",
+			expectedErr:     persistence.ErrMalformedPageToken,
+			expectedPerPage: defaultPageSize,
 		},
 	} {
 		t.Run(fmt.Sprintf("case=%d/size:%d token:%s", i, tc.size, tc.token), func(t *testing.T) {
 			pagination, err := internalPaginationFromOptions(x.WithSize(tc.size), x.WithToken(tc.token))
 
 			assert.True(t, errors.Is(err, tc.expectedErr))
-			assert.Equal(t, tc.expectedLimit, pagination.Limit)
-			assert.Equal(t, tc.expectedOffset, pagination.Offset)
+			assert.Equal(t, tc.expectedPerPage, pagination.PerPage)
+			assert.Equal(t, tc.expectedPage, pagination.Page)
 		})
 	}
 }
