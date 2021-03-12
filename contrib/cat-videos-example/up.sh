0ad8bd8ab8dfd3a6dc518bfc7bb97c83c500df37
zepatrik <zepatrik@users.noreply.github.com>
2021-03-12 17:36:19 +0100

    docs: fix quickstart and details

--- contrib/cat-videos-example/up.sh
+++ contrib/cat-videos-example/up.sh
@@ -9,18 +9,19 @@ function teardown() {
 }
 trap teardown EXIT
 
-export KETO_GRPC_URL="127.0.0.1:4467"
+export KETO_WRITE_REMOTE="127.0.0.1:4467"
 
 keto relation-tuple create contrib/cat-videos-example/relation-tuples
 
 echo "
 
-Created all relation tuples. Now you can use the Keto CLI client like so:
+Created all relation tuples. Now you can use the Keto CLI client to play around:
 
-export KETO_GRPC_URL=\"127.0.0.1:4467\"
+export KETO_READ_REMOTE=\"127.0.0.1:4466\"
 keto relation-tuple get videos
 keto check \"*\" view videos /cats/1.mp4
 keto expand view videos /cats/2.mp4
 "
 
-sleep 10d
+# sleep 10h; has to be defined like this because OSX does not know units https://www.unix.com/man-page/osx/1/sleep/
+sleep 36000
