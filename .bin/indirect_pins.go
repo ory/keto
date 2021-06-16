9a675f022294fe1386a1e77bf13a644eebb36ac3
Patrik <zepatrik@users.noreply.github.com>
2021-06-16 09:48:44 +0200

    chore: split dev dependencies and code dependencies (#619)
    Co-authored-by: aeneasr <3372410+aeneasr@users.noreply.github.com>

---
+++ .bin/indirect_pins.go
@@ -0,0 +1,21 @@
+// +build go_mod_indirect_pins
+
+package _bin
+
+import (
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"
+	_ "github.com/mattn/goveralls"
+	_ "golang.org/x/tools/cmd/goimports"
+
+	_ "github.com/ory/cli"
+	_ "github.com/ory/go-acc"
+
+	// Protobuf and gRPC related tools
+	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+
+	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
+
+	_ "github.com/goreleaser/godownloader"
+)
