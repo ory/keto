9e5410d8ec263c07a01764519bc75396137e186b
zepatrik <zepatrik@users.noreply.github.com>
2021-08-31 15:28:22 +0200

    fix(cli): panic when printing nil expand trees

--- cmd/expand/root_test.go
+++ cmd/expand/root_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/ory/x/cmdx"
+
 	"github.com/stretchr/testify/assert"
 
 	"github.com/ory/keto/cmd/client"
@@ -15,7 +16,15 @@ func TestExpandCommand(t *testing.T) {
 	ts := client.NewTestServer(t, client.ReadServer, []*namespace.Namespace{nspace}, NewExpandCmd)
 	defer ts.Shutdown(t)
 
-	ts.Cmd.PersistentArgs = append(ts.Cmd.PersistentArgs, "--"+cmdx.FlagFormat, string(cmdx.FormatJSON))
-	stdOut := ts.Cmd.ExecNoErr(t, "access", nspace.Name, "object")
-	assert.Equal(t, "null\n", stdOut)
+	t.Run("case=unknown tuple", func(t *testing.T) {
+		t.Run("format=JSON", func(t *testing.T) {
+			stdOut := ts.Cmd.ExecNoErr(t, "access", nspace.Name, "object", "--"+cmdx.FlagFormat, string(cmdx.FormatJSON))
+			assert.Equal(t, "null\n", stdOut)
+		})
+
+		t.Run("format=default", func(t *testing.T) {
+			stdOut := ts.Cmd.ExecNoErr(t, "access", nspace.Name, "object", "--"+cmdx.FlagFormat, string(cmdx.FormatDefault))
+			assert.Contains(t, stdOut, "empty tree")
+		})
+	})
 }
