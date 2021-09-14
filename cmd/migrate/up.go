1b9abbd8a9979fb61c835efe86fa111a6d9730e8
zepatrik <zepatrik@users.noreply.github.com>
2021-09-14 15:04:20 +0200

    chore: up migration command: simplify and improve docs

--- cmd/migrate/up.go
+++ cmd/migrate/up.go
@@ -23,8 +23,15 @@ func newUpCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "up",
 		Short: "Migrate the database up",
-		Long: "Migrate the database up.\n" +
-			"This does not affect namespaces. Use `keto namespace migrate up` for migrating namespaces.",
+		Long: `Run this command on a fresh SQL installation and when you upgrade Ory Keto to a new minor version.
+
+It is recommended to run this command close to the SQL instance (e.g. same subnet) instead of over the public internet.
+This decreases risk of failure and decreases time required.
+
+### WARNING ###
+
+Before running this command on an existing database, create a back up!
+`,
 		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			ctx := cmd.Context()
@@ -39,7 +46,7 @@ func newUpCmd() *cobra.Command {
 				return err
 			}
 
-			if err := BoxUp(cmd, mb, ""); err != nil {
+			if err := BoxUp(cmd, mb); err != nil {
 				return err
 			}
 
@@ -58,38 +65,38 @@ func RegisterYesFlag(flags *pflag.FlagSet) {
 	flags.BoolP(FlagYes, "y", false, "yes to all questions, no user input required")
 }
 
-func BoxUp(cmd *cobra.Command, mb *popx.MigrationBox, msgPrefix string) error {
-	_, _ = fmt.Fprintln(cmd.OutOrStdout(), msgPrefix+"Current status:")
+func BoxUp(cmd *cobra.Command, mb *popx.MigrationBox) error {
+	_, _ = fmt.Fprintln(cmd.OutOrStdout(), "Current status:")
 
 	s, err := mb.Status(cmd.Context())
 	if err != nil {
-		_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "%sCould not get migration status: %+v\n", msgPrefix, err)
+		_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "Could not get migration status: %+v\n", err)
 		return cmdx.FailSilently(cmd)
 	}
 	cmdx.PrintTable(cmd, s)
 
 	if !s.HasPending() {
-		_, _ = fmt.Fprintln(cmd.OutOrStdout(), msgPrefix+"All migrations are already applied, there is nothing to do.")
+		_, _ = fmt.Fprintln(cmd.OutOrStdout(), "All migrations are already applied, there is nothing to do.")
 		return nil
 	}
 
-	if !flagx.MustGetBool(cmd, FlagYes) && !cmdx.AskForConfirmation(msgPrefix+"Are you sure that you want to apply this migration? Make sure to check the CHANGELOG.md for breaking changes beforehand.", cmd.InOrStdin(), cmd.OutOrStdout()) {
-		_, _ = fmt.Fprintln(cmd.OutOrStdout(), msgPrefix+"Aborting")
+	if !flagx.MustGetBool(cmd, FlagYes) && !cmdx.AskForConfirmation("Are you sure that you want to apply this migration? Make sure to check the CHANGELOG.md for breaking changes beforehand.", cmd.InOrStdin(), cmd.OutOrStdout()) {
+		_, _ = fmt.Fprintln(cmd.OutOrStdout(), "Aborting")
 		return nil
 	}
 
-	_, _ = fmt.Fprintln(cmd.OutOrStdout(), msgPrefix+"Applying migrations...")
+	_, _ = fmt.Fprintln(cmd.OutOrStdout(), "Applying migrations...")
 
 	if err := mb.Up(cmd.Context()); err != nil {
-		_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "%sCould not apply migrations: %+v\n", msgPrefix, err)
+		_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "%sCould not apply migrations: %+v\n", err)
 		return cmdx.FailSilently(cmd)
 	}
 
-	_, _ = fmt.Fprintln(cmd.OutOrStdout(), msgPrefix+"Successfully applied all migrations:")
+	_, _ = fmt.Fprintln(cmd.OutOrStdout(), "Successfully applied all migrations:")
 
 	s, err = mb.Status(cmd.Context())
 	if err != nil {
-		_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "%sCould not get migration status: %+v\n", msgPrefix, err)
+		_, _ = fmt.Fprintf(cmd.ErrOrStderr(), "Could not get migration status: %+v\n", err)
 		return cmdx.FailSilently(cmd)
 	}
 
