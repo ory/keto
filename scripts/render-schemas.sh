d08ef6612736a40a7739f10628fe82996a4fabc3
Patrik <zepatrik@users.noreply.github.com>
2021-10-01 13:50:17 +0200

    ci: release automation fixes (#748)

--- scripts/render-schemas.sh
+++ scripts/render-schemas.sh
@@ -8,6 +8,9 @@ sed "s!ory://tracing-config!https://raw.githubusercontent.com/ory/x/$ory_x_versi
 s!ory://logging-config!https://raw.githubusercontent.com/ory/x/$ory_x_version/logrusx/config.schema.json!g" internal/driver/config/config.schema.json > .schema/config.schema.json
 
 git add .schema/config.schema.json
-git commit -m "autogen: render config schema"
 
-git push
+if ! git diff --exit-code .schema/config.schema.json
+then
+  git commit -m "autogen: render config schema"
+  git push
+fi
