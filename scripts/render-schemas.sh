ad5d34e29aeaf00a3110ba7daf6f1d2f3b7e01d4
zepatrik <zepatrik@users.noreply.github.com>
2021-10-01 11:18:21 +0200

    ci: fix pre-release render schema script

--- scripts/render-schemas.sh
+++ scripts/render-schemas.sh
@@ -7,8 +7,7 @@ ory_x_version="$(go list -f '{{.Version}}' -m github.com/ory/x)"
 sed "s!ory://tracing-config!https://raw.githubusercontent.com/ory/x/$ory_x_version/tracing/config.schema.json!g;
 s!ory://logging-config!https://raw.githubusercontent.com/ory/x/$ory_x_version/logrusx/config.schema.json!g" internal/driver/config/config.schema.json > .schema/config.schema.json
 
-git config user.email "zepatrik@users.noreply.github.com"
-git config user.name "zepatrik"
-
 git add .schema/config.schema.json
 git commit -m "autogen: render config schema"
+
+git push origin
