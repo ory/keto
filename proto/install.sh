20cf2418a9883bd6122bc34271152c30ab79eb36
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2021-12-06 18:32:17 +0000

    Add installation instructions to proto README

--- proto/install.sh
+++
@@ -1,5 +0,0 @@
-PB_REL="https://github.com/protocolbuffers/protobuf/releases"
-PB_VERION="3.13.0"
-curl -LO $PB_REL/download/v${PB_VERION}/protoc-${PB_VERION}-linux-x86_64.zip
-unzip protoc-${PB_VERION}-linux-x86_64.zip -d $HOME/.local
-export PATH="$PATH:$HOME/.local/bin"
