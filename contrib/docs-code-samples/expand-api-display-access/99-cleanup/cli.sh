2d4ce93d10b72ee29367a6a89197d7dcb7d0e9f3
Patrik <patrik@ory.sh>
2022-01-21 15:01:08 +0100

    fix: code samples

--- contrib/docs-code-samples/expand-api-display-access/99-cleanup/cli.sh
+++ contrib/docs-code-samples/expand-api-display-access/99-cleanup/cli.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 export KETO_READ_REMOTE="127.0.0.1:4466"
 export KETO_WRITE_REMOTE="127.0.0.1:4467"
 
-keto relation-tuple get files --format json | \
+keto relation-tuple get --namespace files --format json | \
   jq ".relation_tuples" | \
     keto relation-tuple delete - -q > /dev/null
 
-keto relation-tuple get directories --format json | \
+keto relation-tuple get --namespace directories --format json | \
   jq ".relation_tuples" | \
     keto relation-tuple delete - -q > /dev/null
