20925a60cbe423ce1559363f06d4271877f23f19
Henning Perl <henning.perl@gmail.com>
2022-01-21 17:44:14 +0100

    feat: Add pagination to the migrator

--- internal/persistence/sql/migrations/uuid_mapping.go
+++ internal/persistence/sql/migrations/uuid_mapping.go
@@ -7,17 +7,19 @@ import (
 	"github.com/gofrs/uuid"
 
 	"github.com/ory/keto/internal/persistence/sql"
+	"github.com/ory/x/sqlcon"
 )
 
 type (
 	toUUIDMappingMigrator struct {
-		d dependencies
+		d       dependencies
+		perPage int
 	}
 )
 
 // NewToUUIDMappingMigrator creates a new UUID mapping migrator.
 func NewToUUIDMappingMigrator(d dependencies) *toUUIDMappingMigrator {
-	return &toUUIDMappingMigrator{d: d}
+	return &toUUIDMappingMigrator{d: d, perPage: 100}
 }
 
 // MigrateUUIDMappings migrates to UUID-mapped subject IDs for all relation
@@ -29,28 +31,48 @@ func (m *toUUIDMappingMigrator) MigrateUUIDMappings(ctx context.Context) error {
 	}
 
 	return p.Transaction(ctx, func(ctx context.Context, c *pop.Connection) error {
-		var relationTuples []*sql.RelationTuple
-
-		if err := p.Connection(ctx).All(&relationTuples); err != nil {
-			return err
-		}
-
-		for _, rt := range relationTuples {
-			_, err := uuid.FromString(rt.SubjectID.String)
-			if err == nil || !rt.SubjectID.Valid || rt.SubjectID.String == "" {
-				continue
-			}
-
-			id := uuid.Must(uuid.NewV4())
-			if err := p.AddUUIDMapping(ctx, id, rt.SubjectID.String); err != nil {
+		for page := 1; ; page++ {
+			relationTuples, hasNext, err := m.getRelationTuples(ctx, page)
+			if err != nil {
 				return err
 			}
 
-			rt.SubjectID.String = id.String()
-			if err := c.Update(rt); err != nil {
+			if err := p.Connection(ctx).All(&relationTuples); err != nil {
 				return err
+			}
+
+			for _, rt := range relationTuples {
+				_, err := uuid.FromString(rt.SubjectID.String)
+				if err == nil || !rt.SubjectID.Valid || rt.SubjectID.String == "" {
+					continue
+				}
+
+				id := uuid.Must(uuid.NewV4())
+				if err := p.AddUUIDMapping(ctx, id, rt.SubjectID.String); err != nil {
+					return err
+				}
+
+				rt.SubjectID.String = id.String()
+				if err := c.Update(rt); err != nil {
+					return err
+				}
+			}
+
+			if !hasNext {
+				break
 			}
 		}
 		return nil
 	})
+}
+
+func (m *toUUIDMappingMigrator) getRelationTuples(ctx context.Context, page int) (res []*sql.RelationTuple, hasNext bool, err error) {
+	q := m.d.Persister().Connection(ctx).
+		Order("nid, shard_id").
+		Paginate(page, m.perPage)
+
+	if err := q.All(&res); err != nil {
+		return nil, false, sqlcon.HandleError(err)
+	}
+	return res, q.Paginator.Page < q.Paginator.TotalPages, nil
 }
