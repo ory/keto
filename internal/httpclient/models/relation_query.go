7e717161f1ce90c132fd4b9ad75aad1804671157
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-01-21 15:38:44 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/models/relation_query.go
+++ internal/httpclient/models/relation_query.go
@@ -59,6 +59,8 @@ func (m *RelationQuery) validateSubjectSet(formats strfmt.Registry) error {
 		if err := m.SubjectSet.Validate(formats); err != nil {
 			if ve, ok := err.(*errors.Validation); ok {
 				return ve.ValidateName("subject_set")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("subject_set")
 			}
 			return err
 		}
@@ -87,6 +89,8 @@ func (m *RelationQuery) contextValidateSubjectSet(ctx context.Context, formats s
 		if err := m.SubjectSet.ContextValidate(ctx, formats); err != nil {
 			if ve, ok := err.(*errors.Validation); ok {
 				return ve.ValidateName("subject_set")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("subject_set")
 			}
 			return err
 		}
