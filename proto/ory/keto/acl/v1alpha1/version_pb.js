81319d752cde6ea9ca24b6a0b85e743f205dbed0
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-08 18:18:21 +0100

    u

--- proto/ory/keto/acl/v1alpha1/version_pb.js
+++ proto/ory/keto/acl/v1alpha1/version_pb.js
@@ -2,15 +2,24 @@
 /**
  * @fileoverview
  * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
  * @suppress {messageConventions} JS Compiler reports an error if a variable or
  *     field starts with 'MSG_' and isn't a translatable message.
  * @public
  */
 // GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
 
 var jspb = require('google-protobuf');
 var goog = jspb;
-var global = Function('return this')();
+var global = (function() {
+  if (this) { return this; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  if (typeof self !== 'undefined') { return self; }
+  return Function('return this')();
+}.call(null));
 
 goog.exportSymbol('proto.ory.keto.acl.v1alpha1.GetVersionRequest', null, global);
 goog.exportSymbol('proto.ory.keto.acl.v1alpha1.GetVersionResponse', null, global);
