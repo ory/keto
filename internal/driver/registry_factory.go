673a15918e1c9fcf89cad4adf05cd4beb9ea547a
Patrik <zepatrik@users.noreply.github.com>
2022-03-01 16:52:29 +0100

    test: set config values before initializing

--- internal/driver/registry_factory.go
+++ internal/driver/registry_factory.go
@@ -2,6 +2,7 @@ package driver
 
 import (
 	"context"
+	"github.com/ory/x/configx"
 	"testing"
 
 	"github.com/ory/keto/ketoctx"
@@ -72,11 +73,12 @@ func NewTestRegistry(t *testing.T, dsn *dbx.DsnT) *RegistryDefault {
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
 
+	ctx = configx.ContextWithConfigOptions(ctx, configx.WithValues(map[string]interface{}{
+		config.KeyDSN: dsn.Conn,
+		"log.level":   "debug",
+	}))
 	c, err := config.NewDefault(ctx, nil, l)
 	require.NoError(t, err)
-
-	require.NoError(t, c.Set(config.KeyDSN, dsn.Conn))
-	require.NoError(t, c.Set("log.level", "debug"))
 
 	r := &RegistryDefault{
 		c:     c,
