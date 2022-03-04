ae63cecb07f8a4da556ddcfe0a7ebb01de723aee
Patrik <zepatrik@users.noreply.github.com>
2022-03-04 11:14:27 +0100

    ci: fix path in schema render script

--- scripts/render-schemas.sh
+++ scripts/render-schemas.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 ory_x_version="$(go list -f '{{.Version}}' -m github.com/ory/x)"
 
 sed "s!ory://tracing-config!https://raw.githubusercontent.com/ory/x/$ory_x_version/tracing/config.schema.json!g;
-s!ory://logging-config!https://raw.githubusercontent.com/ory/x/$ory_x_version/logrusx/config.schema.json!g" internal/driver/config/config.schema.json > .schema/config.schema.json
+s!ory://logging-config!https://raw.githubusercontent.com/ory/x/$ory_x_version/logrusx/config.schema.json!g" embedx/config.schema.json > .schema/config.schema.json
 
 git add .schema/config.schema.json
 
