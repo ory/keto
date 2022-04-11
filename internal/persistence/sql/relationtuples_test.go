9384cb2a4860d26a8edd90d2457f0f05e99f06da
Ajay Kelkar <ajaykelkar@outlook.com>
2022-04-11 19:14:02 +0200

    test: adding a test for persister query count

--- internal/persistence/sql/relationtuples_test.go
+++ internal/persistence/sql/relationtuples_test.go
@@ -3,6 +3,9 @@ package sql_test
 import (
 	"context"
 	stdSql "database/sql"
+	"github.com/ory/keto/internal/driver/config"
+	"github.com/ory/keto/internal/namespace"
+	"github.com/ory/keto/internal/relationtuple"
 	"strings"
 	"testing"
 	"time"
@@ -104,6 +107,70 @@ func TestRelationTupleSubjectTypeCheck(t *testing.T) {
 							strings.Contains(err.Error(), "chk_keto_rt_subject_type") || // <- normal databases
 								strings.Contains(err.Error(), "SQLSTATE 23514")) // <- mysql
 					}
+				})
+			}
+		})
+	}
+}
+
+func TestPersister_GetRelationTuplesCount(t *testing.T) {
+	nid := uuid.Must(uuid.FromString("d59f09fc-dc9e-4733-800e-bb41aba6b1ea"))
+	ns := &namespace.Namespace{ID: 1, Name: "cards"}
+
+	for _, dsn := range dbx.GetDSNs(t, false) {
+		t.Run("dsn="+dsn.Name, func(t *testing.T) {
+			ctx := context.Background()
+			reg := driver.NewTestRegistry(t, dsn)
+			require.NoError(t,
+				reg.Config(ctx).Set(config.KeyNamespaces, []*namespace.Namespace{ns}))
+
+			rts := []*relationtuple.InternalRelationTuple{
+				{
+					Namespace: ns.Name,
+					Object:    "Spade1",
+					Relation:  "r1",
+					Subject:   &relationtuple.SubjectID{ID: "s1"},
+				},
+				{
+					Namespace: ns.Name,
+					Object:    "Hearts1",
+					Relation:  "r2",
+					Subject:   &relationtuple.SubjectID{ID: "s2"},
+				},
+			}
+
+			err := reg.RelationTupleManager().WriteRelationTuples(context.Background(), rts...)
+			assert.NoError(t, err)
+
+			for _, tc := range []struct {
+				desc          string
+				query         *relationtuple.RelationQuery
+				expectedCount int
+			}{
+				{
+					desc: "Get all records count",
+					query: &relationtuple.RelationQuery{
+						Namespace: "cards",
+					},
+					expectedCount: 2,
+				},
+				{
+					desc: "get count with query defined",
+					query: &relationtuple.RelationQuery{
+						Namespace: "cards",
+						Relation:  "r1",
+					},
+					expectedCount: 1,
+				},
+			} {
+				t.Run("case="+tc.desc, func(t *testing.T) {
+					p, err := sql.NewPersister(ctx, reg, nid)
+					require.NoError(t, err)
+
+					count, err := p.GetRelationTuplesCount(ctx, tc.query)
+
+					assert.NoError(t, err)
+					assert.Equal(t, tc.expectedCount, *count)
 				})
 			}
 		})
