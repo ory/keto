890d1588b81abc238eca45d816e2b3f3da89d39e
hperl <34397+hperl@users.noreply.github.com>
2022-05-27 16:30:37 +0200

    feat: userset rewrite set intersection, difference

--- internal/check/userset_rewrites_test.go
+++ internal/check/userset_rewrites_test.go
@@ -14,39 +14,76 @@ import (
 	"github.com/ory/keto/internal/relationtuple"
 )
 
-var docsNS = namespace.Namespace{Name: "docs",
-	ID: 1,
-	Relations: []ast.Relation{
-		{
-			Name: "owner",
-		},
-		{
-			Name: "editor",
-			UsersetRewrite: &ast.UsersetRewrite{
-				Children: ast.Children{
-					ComputedUsersets: []ast.ComputedUserset{
-						{
-							Relation: "owner",
-						},
-					},
-				},
-			},
-		},
-		{
-			Name: "viewer",
-			UsersetRewrite: &ast.UsersetRewrite{
-				Children: ast.Children{
-					ComputedUsersets: []ast.ComputedUserset{{
-						Relation: "editor",
-					}},
-					TupleToUsersets: []ast.TupleToUserset{{
-						Relation:                "parent",
-						ComputedUsersetRelation: "viewer",
-					}},
-				},
-			},
-		},
+var namespaces = []*namespace.Namespace{
+	{Name: "doc",
+		ID: 1,
+		Relations: []ast.Relation{
+			{
+				Name: "owner"},
+			{
+				Name: "editor",
+				UsersetRewrite: &ast.UsersetRewrite{
+					Children: ast.Children{ast.ComputedUserset{
+						Relation: "owner"}}}},
+			{
+				Name: "viewer",
+				UsersetRewrite: &ast.UsersetRewrite{
+					Children: ast.Children{
+						ast.ComputedUserset{
+							Relation: "editor"},
+						ast.TupleToUserset{
+							Relation:                "parent",
+							ComputedUsersetRelation: "viewer"}}}},
+		}},
+	{Name: "group",
+		ID:        2,
+		Relations: []ast.Relation{{Name: "member"}},
 	},
+	{Name: "level",
+		ID:        3,
+		Relations: []ast.Relation{{Name: "member"}},
+	},
+	{Name: "resource",
+		ID: 4,
+		Relations: []ast.Relation{
+			{Name: "level"},
+			{Name: "viewer",
+				UsersetRewrite: &ast.UsersetRewrite{
+					Children: ast.Children{
+						ast.TupleToUserset{Relation: "owner", ComputedUsersetRelation: "member"}}}},
+			{Name: "owner",
+				UsersetRewrite: &ast.UsersetRewrite{
+					Children: ast.Children{
+						ast.TupleToUserset{Relation: "owner", ComputedUsersetRelation: "member"}}}},
+			{Name: "read",
+				UsersetRewrite: &ast.UsersetRewrite{
+					Children: ast.Children{
+						ast.ComputedUserset{Relation: "viewer"},
+						ast.ComputedUserset{Relation: "owner"}}}},
+			{Name: "update",
+				UsersetRewrite: &ast.UsersetRewrite{
+					Children: ast.Children{
+						ast.ComputedUserset{Relation: "owner"}}}},
+			{Name: "delete",
+				UsersetRewrite: &ast.UsersetRewrite{
+					Operation: ast.SetOperationIntersection,
+					Children: ast.Children{
+						ast.ComputedUserset{Relation: "owner"},
+						ast.TupleToUserset{
+							Relation:                "level",
+							ComputedUsersetRelation: "member"}}}},
+		}},
+	{Name: "acl",
+		ID: 5,
+		Relations: []ast.Relation{
+			{Name: "allow"},
+			{Name: "deny"},
+			{Name: "access",
+				UsersetRewrite: &ast.UsersetRewrite{
+					Operation: ast.SetOperationDifference,
+					Children: ast.Children{
+						ast.ComputedUserset{Relation: "allow"},
+						ast.ComputedUserset{Relation: "deny"}}}}}},
 }
 
 func insertFixtures(t *testing.T, m relationtuple.Manager, tuples []string) {
@@ -60,28 +97,35 @@ func insertFixtures(t *testing.T, m relationtuple.Manager, tuples []string) {
 	require.NoError(t, m.WriteRelationTuples(context.Background(), relationTuples...))
 }
 
-func TestUsersetRewrites_DocsExample(t *testing.T) {
+func TestUsersetRewrites(t *testing.T) {
 	ctx := context.Background()
 
-	reg := newDepsProvider(t, []*namespace.Namespace{&docsNS})
+	reg := newDepsProvider(t, namespaces)
 	reg.Logger().Logger.SetLevel(logrus.TraceLevel)
-	nsMgr, err := reg.Config(ctx).NamespaceManager()
-	require.NoError(t, err)
-	ns, err := nsMgr.GetNamespaceByName(ctx, docsNS.Name)
-	require.NoError(t, err)
-	require.Equal(t, &docsNS, ns)
 
 	insertFixtures(t, reg.RelationTupleManager(), []string{
-		"docs:document#owner@user",                  // user owns doc
-		"docs:doc_in_folder#parent@docs:folder#...", // doc_in_folder is in folder
-		"docs:folder#owner@user",                    // user owns folder
+		"doc:document#owner@user",                 // user owns doc
+		"doc:doc_in_folder#parent@doc:folder#...", // doc_in_folder is in folder
+		"doc:folder#owner@user",                   // user owns folder
 
 		// Folder hierarchy folder_a -> folder_b -> folder_c -> file
 		// and folder_a is owned by user. Then user should have access to file.
-		"docs:file#parent@docs:folder_c#...",
-		"docs:folder_c#parent@docs:folder_b#...",
-		"docs:folder_b#parent@docs:folder_a#...",
-		"docs:folder_a#owner@user",
+		"doc:file#parent@doc:folder_c#...",
+		"doc:folder_c#parent@doc:folder_b#...",
+		"doc:folder_b#parent@doc:folder_a#...",
+		"doc:folder_a#owner@user",
+
+		"group:editors#member@mark",
+		"level:superadmin#member@mark",
+		"level:superadmin#member@sandy",
+		"resource:topsecret#owner@group:editors#...",
+		"resource:topsecret#level@level:superadmin#...",
+		"resource:topsecret#owner@mike",
+
+		"acl:document#allow@alice",
+		"acl:document#allow@bob",
+		"acl:document#allow@mallory",
+		"acl:document#deny@mallory",
 	})
 
 	e := check.NewEngine(reg)
@@ -91,37 +135,67 @@ func TestUsersetRewrites_DocsExample(t *testing.T) {
 		allowed bool
 	}{{
 		// direct
-		query:   "docs:document#owner@user",
+		query:   "doc:document#owner@user",
 		allowed: true,
 	}, {
 		// userset rewrite
-		query:   "docs:document#editor@user",
+		query:   "doc:document#editor@user",
 		allowed: true,
 	}, {
 		// transitive userset rewrite
-		query:   "docs:document#viewer@user",
+		query:   "doc:document#viewer@user",
 		allowed: true,
 	}, {
-		query:   "docs:document#editor@nobody",
+		query:   "doc:document#editor@nobody",
 		allowed: false,
 	}, {
-		query:   "docs:folder#viewer@user",
+		query:   "doc:folder#viewer@user",
 		allowed: true,
 	}, {
 		// tuple to userset
-		query:   "docs:doc_in_folder#viewer@user",
+		query:   "doc:doc_in_folder#viewer@user",
 		allowed: true,
 	}, {
 		// tuple to userset
-		query:   "docs:doc_in_folder#viewer@nobody",
+		query:   "doc:doc_in_folder#viewer@nobody",
 		allowed: false,
 	}, {
 		// tuple to userset
-		query:   "docs:another_doc#viewer@user",
+		query:   "doc:another_doc#viewer@user",
 		allowed: false,
 	}, {
-		query:   "docs:file#viewer@user",
+		query:   "doc:file#viewer@user",
 		allowed: true,
+	}, {
+		query:   "level:superadmin#member@mark",
+		allowed: true, // mark is both editor and has correct level
+	}, {
+		query:   "resource:topsecret#owner@mark",
+		allowed: true, // mark is both editor and has correct level
+	}, {
+		query:   "resource:topsecret#delete@mark",
+		allowed: true, // mark is both editor and has correct level
+	}, {
+		query:   "resource:topsecret#update@mike",
+		allowed: true, // mike owns the resource
+	}, {
+		query:   "resource:topsecret#delete@mike",
+		allowed: false, // mike does not have correct level
+	}, {
+		query:   "resource:topsecret#delete@sandy",
+		allowed: false, // sandy is not in the editor group
+	}, {
+		query:   "acl:document#access@alice",
+		allowed: true,
+	}, {
+		query:   "acl:document#access@bob",
+		allowed: true,
+	}, {
+		query:   "acl:document#allow@mallory",
+		allowed: true,
+	}, {
+		query:   "acl:document#access@mallory",
+		allowed: false, // mallory is also on deny-list
 	}}
 
 	for _, tc := range testCases {
