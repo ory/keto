b80a230493eabf5056111ce59959c7d0238465fd
Henning Perl <henning.perl@gmail.com>
2022-10-10 10:26:45 +0200

    feat: expose function to generate OPL (#1057)
    Co-authored-by: Patrik <zepatrik@users.noreply.github.com>

---
+++ cmd/namespace/opl_generate_test.go
@@ -0,0 +1,35 @@
+package namespace_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/ory/x/snapshotx"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ory/keto/cmd/namespace"
+)
+
+func TestGenerateOPLConfig(t *testing.T) {
+	cases := []struct {
+		name       string
+		namespaces []string
+	}{{
+		name:       "empty",
+		namespaces: []string{},
+	}, {
+		name:       "one",
+		namespaces: []string{"one"},
+	}, {
+		name:       "many",
+		namespaces: []string{"one", "two", "three"},
+	}}
+
+	for _, tc := range cases {
+		t.Run("case="+tc.name, func(t *testing.T) {
+			var out bytes.Buffer
+			require.NoError(t, namespace.GenerateOPLConfig(tc.namespaces, &out))
+			snapshotx.SnapshotT(t, out.String())
+		})
+	}
+}
