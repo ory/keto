17f10effbcc5f45b82093d2c8ec7b2962a3659bf
Patrik <zepatrik@users.noreply.github.com>
2022-10-27 17:39:46 +0200

    feat: allow setting the authority header in the CLI

--- cmd/client/grpc_client.go
+++ cmd/client/grpc_client.go
@@ -28,34 +28,46 @@ const (
 
 	FlagInsecureNoTransportSecurity  = "insecure-disable-transport-security"
 	FlagInsecureSkipHostVerification = "insecure-skip-hostname-verification"
+	FlagAuthority                    = "authority"
 
 	EnvReadRemote  = "KETO_READ_REMOTE"
 	EnvWriteRemote = "KETO_WRITE_REMOTE"
 	EnvAuthToken   = "KETO_BEARER_TOKEN" // nosec G101 -- just the key, not the value
+	EnvAuthority   = "KETO_AUTHORITY"
 
 	ContextKeyTimeout contextKeys = "timeout"
 )
 
-type securityFlags struct {
+type connectionDetails struct {
+	token, authority     string
 	skipHostVerification bool
 	noTransportSecurity  bool
 }
 
-func (sf *securityFlags) transportCredentials() grpc.DialOption {
-	switch {
-	case sf.noTransportSecurity:
-		return grpc.WithTransportCredentials(insecure.NewCredentials())
+func (d *connectionDetails) dialOptions() (opts []grpc.DialOption) {
+	if d.token != "" {
+		opts = append(opts,
+			grpc.WithPerRPCCredentials(
+				oauth.NewOauthAccess(&oauth2.Token{AccessToken: d.token})))
+	}
+	if d.authority != "" {
+		opts = append(opts, grpc.WithAuthority(d.authority))
+	}
 
-	case sf.skipHostVerification:
-		return grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
+	// TLS settings
+	switch {
+	case d.noTransportSecurity:
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	case d.skipHostVerification:
+		opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
 			// nolint explicity set through scary flag
 			InsecureSkipVerify: true,
-		}))
-
+		})))
 	default:
 		// Defaults to the default host root CA bundle
-		return grpc.WithTransportCredentials(credentials.NewTLS(nil))
+		opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(nil)))
 	}
+	return opts
 }
 
 func getRemote(cmd *cobra.Command, flagRemote, envRemote string) (remote string) {
@@ -77,12 +89,17 @@ func getRemote(cmd *cobra.Command, flagRemote, envRemote string) (remote string)
 	return remote
 }
 
-func getToken(cmd *cobra.Command) (token string) {
-	return os.Getenv(EnvAuthToken)
+func getAuthority(cmd *cobra.Command) string {
+	if cmd.Flags().Changed(FlagAuthority) {
+		return flagx.MustGetString(cmd, FlagAuthority)
+	}
+	return os.Getenv(EnvAuthority)
 }
 
-func getSecurityFlags(cmd *cobra.Command) securityFlags {
-	return securityFlags{
+func getConnectionDetails(cmd *cobra.Command) connectionDetails {
+	return connectionDetails{
+		token:                os.Getenv(EnvAuthToken),
+		authority:            getAuthority(cmd),
 		skipHostVerification: flagx.MustGetBool(cmd, FlagInsecureSkipHostVerification),
 		noTransportSecurity:  flagx.MustGetBool(cmd, FlagInsecureNoTransportSecurity),
 	}
@@ -91,20 +108,18 @@ func getSecurityFlags(cmd *cobra.Command) securityFlags {
 func GetReadConn(cmd *cobra.Command) (*grpc.ClientConn, error) {
 	return Conn(cmd.Context(),
 		getRemote(cmd, FlagReadRemote, EnvReadRemote),
-		getToken(cmd),
-		getSecurityFlags(cmd),
+		getConnectionDetails(cmd),
 	)
 }
 
 func GetWriteConn(cmd *cobra.Command) (*grpc.ClientConn, error) {
 	return Conn(cmd.Context(),
 		getRemote(cmd, FlagWriteRemote, EnvWriteRemote),
-		getToken(cmd),
-		getSecurityFlags(cmd),
+		getConnectionDetails(cmd),
 	)
 }
 
-func Conn(ctx context.Context, remote, token string, security securityFlags) (*grpc.ClientConn, error) {
+func Conn(ctx context.Context, remote string, details connectionDetails) (*grpc.ClientConn, error) {
 	timeout := 3 * time.Second
 	if d, ok := ctx.Value(ContextKeyTimeout).(time.Duration); ok {
 		timeout = d
@@ -113,23 +128,20 @@ func Conn(ctx context.Context, remote, token string, security securityFlags) (*g
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	dialOpts := []grpc.DialOption{
-		grpc.WithBlock(),
-		grpc.WithDisableHealthCheck(),
-	}
-	dialOpts = append(dialOpts, security.transportCredentials())
-	if token != "" {
-		dialOpts = append(dialOpts,
-			grpc.WithPerRPCCredentials(
-				oauth.NewOauthAccess(&oauth2.Token{AccessToken: token})))
-	}
-
-	return grpc.DialContext(ctx, remote, dialOpts...)
+	return grpc.DialContext(
+		ctx,
+		remote,
+		append([]grpc.DialOption{
+			grpc.WithBlock(),
+			grpc.WithDisableHealthCheck(),
+		}, details.dialOptions()...)...,
+	)
 }
 
 func RegisterRemoteURLFlags(flags *pflag.FlagSet) {
 	flags.String(FlagReadRemote, "127.0.0.1:4466", "Remote address of the read API endpoint.")
 	flags.String(FlagWriteRemote, "127.0.0.1:4467", "Remote address of the write API endpoint.")
+	flags.String(FlagAuthority, "", "Set the authority header for the remote gRPC server.")
 	flags.Bool(FlagInsecureNoTransportSecurity, false, "Disables transport security. Do not use this in production.")
 	flags.Bool(FlagInsecureSkipHostVerification, false, "Disables hostname verification. Do not use this in production.")
 }
