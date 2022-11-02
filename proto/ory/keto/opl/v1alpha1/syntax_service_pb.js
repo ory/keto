695d512e919cdd9f8fd9ff4782aeb27060ca0f77
Patrik <zepatrik@users.noreply.github.com>
2022-11-02 10:58:59 +0100

    chore: dev dependency and tooling upgrades (#1097)

--- proto/ory/keto/opl/v1alpha1/syntax_service_pb.js
+++ proto/ory/keto/opl/v1alpha1/syntax_service_pb.js
@@ -2,15 +2,24 @@
 /**
  * @fileoverview
  * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
  * @suppress {messageConventions} JS Compiler reports an error if a variable or
  *     field starts with 'MSG_' and isn't a translatable message.
  * @public
  */
 // GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
 
 var jspb = require('google-protobuf');
 var goog = jspb;
-var global = Function('return this')();
+var global =
+    (typeof globalThis !== 'undefined' && globalThis) ||
+    (typeof window !== 'undefined' && window) ||
+    (typeof global !== 'undefined' && global) ||
+    (typeof self !== 'undefined' && self) ||
+    (function () { return this; }).call(null) ||
+    Function('return this')();
 
 goog.exportSymbol('proto.ory.keto.opl.v1alpha1.CheckRequest', null, global);
 goog.exportSymbol('proto.ory.keto.opl.v1alpha1.CheckResponse', null, global);
