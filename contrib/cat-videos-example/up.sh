f048408c03cba8e8d5363a47e81f62250147bc23
gousse <gousse@users.noreply.github.com>
2022-12-02 09:52:43 +0100

    Update up.sh
    fix errors due to not using TLS on the example. Need to add --insecure-disable-transport-security option to command lines

--- contrib/cat-videos-example/up.sh
+++ contrib/cat-videos-example/up.sh
@@ -11,16 +11,16 @@ trap teardown EXIT
 
 export KETO_WRITE_REMOTE="127.0.0.1:4467"
 
-keto relation-tuple create contrib/cat-videos-example/relation-tuples
+keto relation-tuple create contrib/cat-videos-example/relation-tuples --insecure-disable-transport-security
 
 echo "
 
 Created all relationships. Now you can use the Keto CLI client to play around:
 
 export KETO_READ_REMOTE=\"127.0.0.1:4466\"
-keto relation-tuple get videos
-keto check \"*\" view videos /cats/1.mp4
-keto expand view videos /cats/2.mp4
+keto relation-tuple get --insecure-disable-transport-security
+keto check \"*\" view videos /cats/1.mp4 --insecure-disable-transport-security
+keto expand view videos /cats/2.mp4 --insecure-disable-transport-security
 "
 
 # sleep 10h; has to be defined like this because OSX does not know units https://www.unix.com/man-page/osx/1/sleep/
