040b3db104692f8b249f1c0476c3ab08384cffff
Patrik <zepatrik@users.noreply.github.com>
2023-09-20 13:28:35 +0200

    fix: missing block flag on migrate status (#1432)

--- cmd/migrate/status.go
+++ cmd/migrate/status.go
@@ -12,12 +12,12 @@ import (
 	"github.com/ory/x/cmdx"
 	"github.com/spf13/cobra"
 
-	"github.com/ory/keto/cmd/client"
 	"github.com/ory/keto/internal/driver"
 	"github.com/ory/keto/ketoctx"
 )
 
 func newStatusCmd(opts []ketoctx.Option) *cobra.Command {
+	block := false
 	cmd := &cobra.Command{
 		Use:   "status",
 		Short: "Get the current migration status",
@@ -25,11 +25,6 @@ func newStatusCmd(opts []ketoctx.Option) *cobra.Command {
 			"This does not affect namespaces. Use `keto namespace migrate status` for migrating namespaces.",
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			ctx := cmd.Context()
-
-			block, err := cmd.Flags().GetBool(client.FlagBlock)
-			if err != nil {
-				return err
-			}
 
 			reg, err := driver.NewDefaultRegistry(ctx, cmd.Flags(), true, opts)
 			if err != nil {
@@ -54,7 +49,12 @@ func newStatusCmd(opts []ketoctx.Option) *cobra.Command {
 						_, _ = fmt.Fprintf(cmd.OutOrStdout(), " - %s\n", m.Name)
 					}
 				}
-				time.Sleep(time.Second)
+				select {
+				case <-ctx.Done():
+					_, _ = fmt.Fprintln(cmd.ErrOrStderr(), "Context was canceled, exiting...")
+					return cmdx.FailSilently(cmd)
+				case <-time.After(time.Second):
+				}
 				s, err = mb.Status(ctx)
 				if err != nil {
 					_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Could not get migration status: %+v\n", err)
@@ -68,6 +68,7 @@ func newStatusCmd(opts []ketoctx.Option) *cobra.Command {
 	}
 
 	cmdx.RegisterFormatFlags(cmd.Flags())
+	cmd.Flags().BoolVar(&block, "block", false, "Block until all migrations have been applied")
 
 	return cmd
 }
