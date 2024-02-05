db15ba9a212bfeb1d8c90136990ff28c6803bce3
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-02-05 22:36:44 +0100

    fix: generation

--- proto/index.d.ts
+++ proto/index.d.ts
@@ -1,5 +1,19 @@
-import * as v1alpha2 from './ory/keto/relation_tuples/v1alpha2/check_service'
+export * from './ory/keto/relation_tuples/v1alpha2/check_service_pb'
+export * from './ory/keto/relation_tuples/v1alpha2/check_service_connect'
 
-declare module '@ory/keto-grpc-client' {
-  export default v1alpha2
-}
+export * from './ory/keto/relation_tuples/v1alpha2/expand_service_pb'
+export * from './ory/keto/relation_tuples/v1alpha2/expand_service_connect'
+
+export * from './ory/keto/relation_tuples/v1alpha2/namespaces_service_pb'
+export * from './ory/keto/relation_tuples/v1alpha2/namespaces_service_connect'
+
+export * from './ory/keto/relation_tuples/v1alpha2/read_service_pb'
+export * from './ory/keto/relation_tuples/v1alpha2/read_service_connect'
+
+export * from './ory/keto/relation_tuples/v1alpha2/relation_tuples_pb'
+
+export * from './ory/keto/relation_tuples/v1alpha2/version_pb'
+export * from './ory/keto/relation_tuples/v1alpha2/version_connect'
+
+export * from './ory/keto/relation_tuples/v1alpha2/write_service_pb'
+export * from './ory/keto/relation_tuples/v1alpha2/write_service_connect'
