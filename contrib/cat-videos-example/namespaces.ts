435cecc5fa42d3cf7fe24e378247ea6a4b984952
Davud Safarov <davudseferli@gmail.com>
2026-02-20 11:01:15 +0100

    docs: update keto-cat-videos-examples to use OPL
    GitOrigin-RevId: 5007b8db9f56e251e808db601ab0b3fb0a37cde7

---
+++ contrib/cat-videos-example/namespaces.ts
@@ -0,0 +1,13 @@
+// Copyright © 2026 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
+import { Context, Namespace, SubjectSet } from "@ory/keto-namespace-types"
+
+class User implements Namespace {}
+
+class Video implements Namespace {
+  related: {
+    view: (User|SubjectSet<Video, "owner">)[]
+    owner: User[]
+  }
+}
